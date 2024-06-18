bc65de3079c17463a77839aab4999eec5be29136
Rafie1715 <115053667+Rafie1715@users.noreply.github.com>
2024-06-18 23:08:01 +0700

    Update build_script.js

--- build/build_script.js
+++ build/build_script.js
@@ -1,37 +1,35 @@
-function formatRupiah(number) {
-    return 'Rp' + number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
 $(document).ready(function() {
     $.ajax({
-        url: 'database.php',
+        url: 'database.php', // Ubah sesuai dengan lokasi skrip PHP Anda
         method: 'GET',
         dataType: 'json',
         success: function(data) {
             var motherboards = data.motherboard;
-            var processors = data.processor;
+            var processors = data.processor; 
             var gpus = data.gpu;
             var rams = data.ram;
+            var powerSupplies = data.powersupply;
+            var cases = data.cases;
 
             // Populate motherboard dropdown
             motherboards.forEach(function(mb) {
-                var optionText = mb.item_name + " | " + formatRupiah(mb.price);
+                var optionText = mb.item_name + " | Rp" + mb.price.toLocaleString('id-ID');
                 $('#motherboard').append('<option value="' + mb.item_id + '" data-socket="' + mb.socket + '">' + optionText + '</option>');
             });
 
             // Populate GPU dropdown
             gpus.forEach(function(gpu) {
-                var optionText = gpu.item_name + " | " + formatRupiah(gpu.price);
+                var optionText = gpu.item_name + " | Rp" + gpu.price.toLocaleString('id-ID');
                 $('#gpu').append('<option value="' + gpu.item_id + '">' + optionText + '</option>');
             });
 
             // Populate RAM dropdown
             rams.forEach(function(ram) {
-                var optionText = ram.item_name + " | " + formatRupiah(ram.price);
+                var optionText = ram.item_name + " | Rp" + ram.price.toLocaleString('id-ID');
                 $('#ram').append('<option value="' + ram.item_id + '">' + optionText + '</option>');
             });
 
-            // Update CPU dropdown based on selected motherboard
+            // Populate CPU dropdown based on selected motherboard
             $('#motherboard').change(function() {
                 var selectedSocket = $(this).find('option:selected').data('socket');
                 $('#cpu').empty().append('<option value="">Select a CPU</option>');
@@ -39,7 +37,7 @@ $(document).ready(function() {
                     $('#cpu').prop('disabled', false);
                     processors.forEach(function(cpu) {
                         if (cpu.socket === selectedSocket) {
-                            var optionText = cpu.item_name + " | " + formatRupiah(cpu.price);
+                            var optionText = cpu.item_name + " | Rp" + cpu.price.toLocaleString('id-ID');
                             $('#cpu').append('<option value="' + cpu.item_id + '">' + optionText + '</option>');
                         }
                     });
@@ -47,6 +45,21 @@ $(document).ready(function() {
                     $('#cpu').prop('disabled', true);
                 }
             });
+
+            // Populate Power Supply dropdown
+            powerSupplies.forEach(function(ps) {
+                var optionText = ps.model + " | Rp" + ps.price.toLocaleString('id-ID');
+                $('#psu').append('<option value="' + ps.item_id + '">' + optionText + '</option>');
+            });
+
+            // Populate Case dropdown
+            cases.forEach(function(caseItem) {
+                var optionText = caseItem.model + " | Rp" + caseItem.price.toLocaleString('id-ID');
+                $('#case').append('<option value="' + caseItem.item_id + '">' + optionText + '</option>');
+            });
+
+            // Enable/disable CPU dropdown based on motherboard selection
+            $('#motherboard').change();
         },
         error: function(xhr, status, error) {
             console.error("AJAX Error: " + status + error);
