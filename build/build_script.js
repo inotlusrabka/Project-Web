3e4522290aad99c199350fe96afa2ac8b9102caf
Alfazi <55661595+Alfazi@users.noreply.github.com>
2024-06-20 08:35:47 +0700

    Update build_script.js
    update foto item

--- build/build_script.js
+++ build/build_script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: 'database.php', // Ubah sesuai dengan lokasi skrip PHP Anda
+        url: 'database.php',
         method: 'GET',
         dataType: 'json',
         success: function(data) {
@@ -14,35 +14,39 @@ $(document).ready(function() {
             // Populate motherboard dropdown
             motherboards.forEach(function(mb) {
                 var optionText = mb.brand + " " + mb.item_name + " | Rp" + mb.price.toLocaleString('id-ID');
-                $('#motherboard').append('<option value="' + mb.item_id + '" data-socket="' + mb.socket + '" data-price="' + mb.price + '">' + optionText + '</option>');
+                $('#motherboard').append('<option value="' + mb.item_id + '" data-socket="' + mb.socket + '" data-price="' + mb.price + '" data-img="' + mb.image_url + '">' + optionText + '</option>');
             });
 
             // Populate GPU dropdown
             gpus.forEach(function(gpu) {
                 var optionText = gpu.brand + " " + gpu.item_name + " | Rp" + gpu.price.toLocaleString('id-ID');
-                $('#gpu').append('<option value="' + gpu.item_id + '" data-price="' + gpu.price + '">' + optionText + '</option>');
+                $('#gpu').append('<option value="' + gpu.item_id + '" data-price="' + gpu.price + '" data-img="' + gpu.image_url + '">' + optionText + '</option>');
             });
 
             // Populate RAM dropdown
             rams.forEach(function(ram) {
                 var optionText = ram.brand + " " + ram.item_name + " | Rp" + ram.price.toLocaleString('id-ID');
-                $('#ram').append('<option value="' + ram.item_id + '" data-price="' + ram.price + '">' + optionText + '</option>');
+                $('#ram').append('<option value="' + ram.item_id + '" data-price="' + ram.price + '" data-img="' + ram.image_url + '">' + optionText + '</option>');
             });
 
             // Populate CPU dropdown based on selected motherboard
             $('#motherboard').change(function() {
                 var selectedSocket = $(this).find('option:selected').data('socket');
+                var selectedImg = $(this).find('option:selected').data('img');
+                $('#motherboard-img').attr('src', selectedImg).show();
+
                 $('#cpu').empty().append('<option value="">Select a CPU</option>');
                 if (selectedSocket) {
                     $('#cpu').prop('disabled', false);
                     processors.forEach(function(cpu) {
                         if (cpu.socket === selectedSocket) {
                             var optionText = cpu.brand + " " + cpu.item_name + " | Rp" + cpu.price.toLocaleString('id-ID');
-                            $('#cpu').append('<option value="' + cpu.item_id + '" data-price="' + cpu.price + '">' + optionText + '</option>');
+                            $('#cpu').append('<option value="' + cpu.item_id + '" data-price="' + cpu.price + '" data-img="' + cpu.image_url + '">' + optionText + '</option>');
                         }
                     });
                 } else {
                     $('#cpu').prop('disabled', true);
+                    $('#cpu-img').hide();
                 }
                 calculateTotalPrice();
             });
@@ -50,23 +54,29 @@ $(document).ready(function() {
             // Populate Power Supply dropdown
             powerSupplies.forEach(function(ps) {
                 var optionText = ps.model + " | Rp" + ps.price.toLocaleString('id-ID');
-                $('#psu').append('<option value="' + ps.item_id + '" data-price="' + ps.price + '">' + optionText + '</option>');
+                $('#psu').append('<option value="' + ps.item_id + '" data-price="' + ps.price + '" data-img="' + ps.image_url + '">' + optionText + '</option>');
             });
 
             // Populate Case dropdown
             cases.forEach(function(caseItem) {
                 var optionText = caseItem.model + " | Rp" + caseItem.price.toLocaleString('id-ID');
-                $('#case').append('<option value="' + caseItem.item_id + '" data-price="' + caseItem.price + '">' + optionText + '</option>');
+                $('#case').append('<option value="' + caseItem.item_id + '" data-price="' + caseItem.price + '" data-img="' + caseItem.image_url + '">' + optionText + '</option>');
             });
 
             // Enable/disable CPU dropdown based on motherboard selection
             $('#motherboard').change();
 
-            // Add change event listeners to all dropdowns to calculate total price
+            // Add change event listeners to all dropdowns to calculate total price and show images
             $('#motherboard, #cpu, #gpu, #ram, #psu, #case').change(function() {
+                var selectedImg = $(this).find('option:selected').data('img');
+                var imgId = $(this).attr('id') + '-img';
+                if (selectedImg) {
+                    $('#' + imgId).attr('src', selectedImg).show();
+                } else {
+                    $('#' + imgId).hide();
+                }
                 calculateTotalPrice();
             });
-
         },
         error: function(xhr, status, error) {
             console.error("AJAX Error: " + status + error);
