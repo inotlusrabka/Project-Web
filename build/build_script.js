c1271db55218d943eca7d7829781ba1509584eb1
Alfazi <55661595+Alfazi@users.noreply.github.com>
2024-06-20 19:20:50 +0700

    Update build_script.js
    watt calcu

--- build/build_script.js
+++ build/build_script.js
@@ -14,19 +14,19 @@ $(document).ready(function() {
             // Populate motherboard dropdown
             motherboards.forEach(function(mb) {
                 var optionText = mb.brand + " " + mb.item_name + " | Rp" + mb.price.toLocaleString('id-ID');
-                $('#motherboard').append('<option value="' + mb.item_id + '" data-socket="' + mb.socket + '" data-price="' + mb.price + '" data-img="' + mb.image_url + '">' + optionText + '</option>');
+                $('#motherboard').append('<option value="' + mb.item_id + '" data-socket="' + mb.socket + '" data-price="' + mb.price + '" data-img="' + mb.image_url + '" data-power="' + mb.power_usage + '">' + optionText + '</option>');
             });
 
             // Populate GPU dropdown
             gpus.forEach(function(gpu) {
                 var optionText = gpu.brand + " " + gpu.item_name + " | Rp" + gpu.price.toLocaleString('id-ID');
-                $('#gpu').append('<option value="' + gpu.item_id + '" data-price="' + gpu.price + '" data-img="' + gpu.image_url + '">' + optionText + '</option>');
+                $('#gpu').append('<option value="' + gpu.item_id + '" data-price="' + gpu.price + '" data-img="' + gpu.image_url + '" data-power="' + gpu.power_usage + '">' + optionText + '</option>');
             });
 
             // Populate RAM dropdown
             rams.forEach(function(ram) {
                 var optionText = ram.brand + " " + ram.item_name + " | Rp" + ram.price.toLocaleString('id-ID');
-                $('#ram').append('<option value="' + ram.item_id + '" data-price="' + ram.price + '" data-img="' + ram.image_url + '">' + optionText + '</option>');
+                $('#ram').append('<option value="' + ram.item_id + '" data-price="' + ram.price + '" data-img="' + ram.image_url + '" data-power="' + ram.power_usage + '">' + optionText + '</option>');
             });
 
             // Populate CPU dropdown based on selected motherboard
@@ -41,7 +41,7 @@ $(document).ready(function() {
                     processors.forEach(function(cpu) {
                         if (cpu.socket === selectedSocket) {
                             var optionText = cpu.brand + " " + cpu.item_name + " | Rp" + cpu.price.toLocaleString('id-ID');
-                            $('#cpu').append('<option value="' + cpu.item_id + '" data-price="' + cpu.price + '" data-img="' + cpu.image_url + '">' + optionText + '</option>');
+                            $('#cpu').append('<option value="' + cpu.item_id + '" data-price="' + cpu.price + '" data-img="' + cpu.image_url + '" data-power="' + cpu.power_usage + '">' + optionText + '</option>');
                         }
                     });
                 } else {
@@ -54,7 +54,7 @@ $(document).ready(function() {
             // Populate Power Supply dropdown
             powerSupplies.forEach(function(ps) {
                 var optionText = ps.model + " | Rp" + ps.price.toLocaleString('id-ID');
-                $('#psu').append('<option value="' + ps.item_id + '" data-price="' + ps.price + '" data-img="' + ps.image_url + '">' + optionText + '</option>');
+                $('#psu').append('<option value="' + ps.item_id + '" data-price="' + ps.price + '" data-img="' + ps.image_url + '" data-power_watt="' + ps.wattage + '">' + optionText + '</option>');
             });
 
             // Populate Case dropdown
@@ -64,7 +64,9 @@ $(document).ready(function() {
             });
 
             // Enable/disable CPU dropdown based on motherboard selection
-            $('#motherboard').change();
+            $('#motherboard').change(function() {
+                calculateTotalPower();
+            });
 
             // Add change event listeners to all dropdowns to calculate total price and show images
             $('#motherboard, #cpu, #gpu, #ram, #psu, #case').change(function() {
@@ -75,6 +77,7 @@ $(document).ready(function() {
                 } else {
                     $('#' + imgId).hide();
                 }
+                calculateTotalPower();
                 calculateTotalPrice();
             });
         },
@@ -82,6 +85,30 @@ $(document).ready(function() {
             console.error("AJAX Error: " + status + error);
         }
     });
+
+    function checkPowerSupply(totalPower) {
+        var selectedPSUWattage = $('#psu').find('option:selected').data('power_watt');
+        if (selectedPSUWattage && totalPower > selectedPSUWattage) {
+            // Handle insufficient power supply
+            $('#power-warning').text('Warning: Selected PSU may not provide sufficient power!');
+        } else {
+            $('#power-warning').text('');
+        }
+
+    }
+    function calculateTotalPower() {
+        var totalPower = 0;
+        $('#motherboard, #cpu, #gpu, #ram').each(function() {
+            var selectedOption = $(this).find('option:selected');
+            var power = selectedOption.data('power');
+            if (power) {
+                totalPower += parseInt(power);
+            }
+        });
+        $('#total-power').text(totalPower + ' W');
+        checkPowerSupply(totalPower);
+    }
+    
 
     function calculateTotalPrice() {
         var totalPrice = 0;
