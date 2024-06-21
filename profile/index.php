5e5a0eb985140c7814c6b25be4b00760758f003a
Rafie1715 <115053667+Rafie1715@users.noreply.github.com>
2024-06-21 07:16:49 +0700

    Update index.php

--- profile/index.php
+++ profile/index.php
@@ -69,6 +69,7 @@
                         <li class="nav-item"><a class="nav-link" href="/ProjekUAS/forum">Forum</a></li>
                         <li class="nav-item"><a class="nav-link" href="/ProjekUAS/#about">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="/ProjekUAS/#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/ProjekUAS/#team">Our Team</a></li>
                         <li class="nav-item"><a class="nav-link" href="/ProjekUAS/profile"><i class="fas fa-user"></i></a></li>
                     </ul>
                 </div>
