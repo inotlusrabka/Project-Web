9a02da03075db0b5a8802984e7dc6ae4de4901e6
Rafie1715 <115053667+Rafie1715@users.noreply.github.com>
2024-06-22 17:25:11 +0700

    Add files via upload

---
+++ profile/update_profile.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header('Location: /ProjekUAS/login.php');
+    exit();
+}
+
+// Konfigurasi koneksi ke database
+$serverName = "localhost";
+$databaseUsername = "root";
+$databasePassword = "rafie1715";
+$databaseName = "pc_part";
+
+// Membuat koneksi ke database
+$connect = new mysqli($serverName, $databaseUsername, $databasePassword, $databaseName);
+
+// Memeriksa koneksi
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+}
+
+// Mendapatkan data dari form
+$username = $_SESSION['username'];
+$email = $_POST['profile-email'];
+$bio = $_POST['profile-bio'];
+
+// Update data pengguna di database
+$sql = "UPDATE userdata SET Email = ?, Bio = ? WHERE Username = ?";
+$stmt = $connect->prepare($sql);
+$stmt->bind_param("sss", $email, $bio, $username);
+
+if ($stmt->execute()) {
+    // Update session variables
+    $_SESSION['email'] = $email;
+    $_SESSION['bio'] = $bio;
+    header('Location: /ProjekUAS/profile'); // Redirect kembali ke halaman profile
+} else {
+    echo "Error updating record: " . $connect->error;
+}
+
+$stmt->close();
+$connect->close();
+?>
+
